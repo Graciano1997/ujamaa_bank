ae8e0e595eb950ef48c64b9b240140f28aa22719
Graciano Henrique <gracianomanuelhenrique@gmail.com>
2024-05-08 09:26:15 +0100

    remove the JS

--- public/assets/js/ips.js
+++
@@ -1,4 +0,0 @@
-  const ipClient = 'http://localhost:3000';
-  const ipServer = 'http://localhost:3001';
-//   const ipServer = 'http://192.168.43.76:3001';
-//   const ipClient = 'http://192.168.43.76:3000';
